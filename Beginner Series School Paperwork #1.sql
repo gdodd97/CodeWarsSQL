0cfd16da2a2cba404cfca49542ccba8cea17a8d1
Gregory Dodd <gdodd97@gmail.com>
2022-09-11 13:54:44 -0400

    Add files via upload

---
+++ Beginner Series School Paperwork #1.sql
@@ -0,0 +1,17 @@
+-- # write your SQL statement here: you are given a table 'paperwork' with columns 'id', n' and 'm', return a table with columns 'n', 'm' and your result in a column named 'res'.
+
+/*********************************
+Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+*********************************/
+
+
+SELECT n,m, 
+CASE
+  WHEN m < 0
+  THEN 0
+  WHEN n < 0
+  THEN 0
+  ELSE m * n END AS res
+FROM paperwork
