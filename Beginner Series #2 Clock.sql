9e29d1e07d00208937a5130c57449b9429981d28
Gregory Dodd <gdodd97@gmail.com>
2022-09-11 14:08:16 -0400

    Add files via upload

---
+++ Beginner Series #2 Clock.sql
@@ -0,0 +1,7 @@
+/***********************************
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+***********************************/
+
+select (h * 3600000) + (m * 60000) + (s * 1000) as res from past
